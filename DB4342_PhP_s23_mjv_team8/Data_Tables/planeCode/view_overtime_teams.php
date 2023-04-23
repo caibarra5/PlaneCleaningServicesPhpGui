7bcc67277fffc238d6e13d58d0010437f2edc00e
darioavo20 <darioavo@gmail.com>
2023-04-23 13:55:21 -0600

    Added overtime teams and changed links to it

---
+++ DB4342_PhP_s23_mjv_team8/Data_Tables/planeCode/view_overtime_teams.php
@@ -0,0 +1,66 @@
+<!--
+/**
+ * CS 4342 Database Management
+ * @author Instruction team Spring and Fall 2020 with contribution from L. Garnica
+ * @version 2.0
+ * Description: The purpose of these file is to provide PhP basic elements for an interface to access a DB. 
+ * Resources: https://getbootstrap.com/docs/4.5/components/alerts/  -- bootstrap examples
+ *
+ * This file retrieves and displays the records of the table Student.
+ */
+-->
+
+
+<?php
+/*
+* Reference for tables: https://getbootstrap.com/docs/4.5/content/tables/
+*/
+
+session_start();
+require_once('../../config.php');
+require_once('../../validate_session.php');
+?>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Importing Bootstrap CSS library https://getbootstrap.com/ -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <?php $sql = "SELECT * FROM overtime_team_planes";
+    if ($result = $conn->query($sql)) {
+    ?>
+        <table class="table" width=50%>
+            <thead>
+                <td> Plane Code</td>
+                <td> Team Code</td>
+            </thead>
+            <tbody>
+                <?php
+                while ($row = $result->fetch_row()) {
+                ?>
+                    <tr>
+                    <td><?php printf("%s", $row[0]); ?></td>
+                        <td><?php printf("%s", $row[1]); ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    <?php
+    }
+    ?>
+    <!-- Link to return to student_menu-->
+    <a href="plane_menu.php">Back to Plane Menu</a><br>
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
